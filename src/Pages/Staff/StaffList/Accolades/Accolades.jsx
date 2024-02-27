6f39ab61c10469848eb200e5b42a0d1855a6e4e6
Morgan Dackermann <morgandackermann@gmail.com>
2024-02-26 18:26:41 -0800

    L6: Complete

---
+++ src/Pages/Staff/StaffList/Accolades/Accolades.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+import AccoladeItem from "./AccoladeItem";
+
+const Accolades = ({ accolades }) => {
+    return (
+        <AccoladesStyled>
+            {accolades.map((accolade, idk) => {
+                return <AccoladeItem key={idk} accolade={accolade} />;
+            })}
+        </AccoladesStyled>
+    );
+};
+
+export default Accolades;
+
+// prop-types
+Accolades.propTypes = {
+    prop: PropTypes.array.isRequired,
+};
+
+const AccoladesStyled = styled.ul`
+    padding-left: 20px;
+    list-style-type: disc;
+`;
